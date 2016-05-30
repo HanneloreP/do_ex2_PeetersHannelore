7dc0fe2fb1381199a250c599c6d87958c1f474b2
Hannelore Peeters <hannelore-peeters@hotmail.com>
2016-05-30 12:47:51 +0200

    Shellscript added

---
+++ ansible.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get -y install ansible sshpass
+
+ln -sf /vagrant/hosts /etc/ansible/hosts
+
+KNOW_HOSTS_FILE="/home/vagrant/.ssh/known_hosts"
+
+echo $KNOW_HOSTS_FILE
+
+rm $KNOW_HOSTS_FILE
+
+ssh-keyscan 192.168.2.3 >> $KNOW_HOSTS_FILE 
+ssh-keyscan 192.168.2.4 >> $KNOW_HOSTS_FILE
+ssh-keyscan 192.168.2.5 >> $KNOW_HOSTS_FILE
+ssh-keyscan localhost >> $KNOW_HOSTS_FILE 
+chown vagrant:vagrant $KNOW_HOSTS_FILE 
