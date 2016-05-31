c60925572d00f2d9b1c2f4e129d5934006565a05
Hannelore Peeters <hannelore-peeters@hotmail.com>
2016-05-31 13:55:38 +0200

    Changes to script and hosts file to fix Exec-bug

--- ansible.sh
+++ ansible.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible sshpass
 
+#soft-link hosts file
 ln -sf /vagrant/hosts /etc/ansible/hosts
+ln -sf /vagrant/ansible.cfg /etc/ansible/ansible.cfg
 
 KNOW_HOSTS_FILE="/home/vagrant/.ssh/known_hosts"
 
+chmod -x /etc/ansible/hosts
+
 echo $KNOW_HOSTS_FILE
 
-rm $KNOW_HOSTS_FILE
+sudo rm $KNOW_HOSTS_FILE
 
-ssh-keyscan 192.168.2.3 >> $KNOW_HOSTS_FILE 
+ssh-keyscan 192.168.2.2 >> $KNOW_HOSTS_FILE 
 ssh-keyscan 192.168.2.4 >> $KNOW_HOSTS_FILE
 ssh-keyscan 192.168.2.5 >> $KNOW_HOSTS_FILE
+ssh-keyscan 192.168.2.6 >> $KNOW_HOSTS_FILE
 ssh-keyscan localhost >> $KNOW_HOSTS_FILE 
-chown vagrant:vagrant $KNOW_HOSTS_FILE 
+chown vagrant:vagrant $KNOW_HOSTS_FILE
+
+cat >> etc/hosts <<EOL
+#vagrant environment nodes
+192.168.2.2 www1
+192.168.2.4 www2
+192.168.2.5 lb
+192.168.2.6 mgr
+EOL
